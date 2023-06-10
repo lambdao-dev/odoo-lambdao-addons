d7f2fd6aa19b8083e60d2be3e494349362b944cf
fah-mili <fanny@lambdao.dev>
2023-06-10 22:14:21 +0200

    [ADD] project_create_sale_order

---
+++ project_create_sale_order/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 len-foss/Financial Way
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Project: Make Billable",
+    "summary": """Project: Make Billable""",
+    "author": "Lambdao, len-foss, Financial Way, Odoo Community Association (OCA)",
+    "website": "https://lambdao.dev",
+    "category": "project",
+    "version": "16.0.0.0.0",
+    "license": "AGPL-3",
+    "depends": ["project"],
+    "data": ["views/project_project.xml"],
+    "application": True,
+}
---
+++ project_create_sale_order/readme/USAGE.rst
@@ -0,0 +1,2 @@
+To use this module, go to a project that is not linked to a sale order (via the configuration menu).
+Click on the button to create a sale order and add the employees you want to invoice.
