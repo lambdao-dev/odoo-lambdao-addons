fd91c9fa28eeb613f8b8daf38fe9feb0ea4c739d
fah-mili <fanny@lambdao.dev>
2023-06-10 22:14:21 +0200

    [ADD] project_create_sale_order

---
+++ project_create_sale_order/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 len-foss/Lambdao
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Project: Make Billable",
+    "summary": """Project: Make Billable""",
+    "author": "Lambdao, len-foss, Odoo Community Association (OCA)",
+    "website": "https://lambdao.dev",
+    "category": "project",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["project", "sale_timesheet"],
+    "data": ["views/project_project.xml"],
+    "application": True,
+}
---
+++ project_create_sale_order/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module provides a button to access the wizard to create a sale order from an existing project.
